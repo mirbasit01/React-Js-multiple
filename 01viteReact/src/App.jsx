24c1b25347d5026d5acaffc6c0830f84d6951b06
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-01-28 14:04:22 +0500

    first

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,18 @@
+import Demo from "./Demo"
+
+
+ 
+
+function App() {
+  const username = "Basit"
+
+  return (
+    <>
+    <Demo />
+    <h1>hy  {username}</h1>
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
