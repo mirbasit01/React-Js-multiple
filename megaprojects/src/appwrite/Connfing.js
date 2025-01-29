86faab7e78218cbb1f8671cf4c31b1e798c9b491
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-01-29 13:53:06 +0500

    Refactor App component and enhance Appwrite Service with CRUD operations and file handling

--- megaprojects/src/appwrite/Connfing.js
+++ megaprojects/src/appwrite/Connfing.js
@@ -58,6 +58,81 @@ export class Service{
             console.log('Appwrite Error ::updataPost :: error',error );
          }
     }
+
+    async DeletPost(slug){
+  try {
+    await this.Databases.deleteDocument(
+      conf.appwriteDataBaseId,
+      conf.appwriteCollectionId,
+      slug
+    ) 
+      return true
+  } catch (error) {
+    console.log('Appwrite Error ::DeletPost :: error',error );
+    return false
+  }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.Databases.getDocument(
+                conf.appwriteDataBaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log('Appwrite Error ::getPost :: error',error );
+            return false
+       }
+    }
+    async getPosts(queries = [Query.equal('status', 'active')]){
+        try {
+            return await this.Databases.listDocuments(
+                conf.appwriteDataBaseId,
+                conf.appwriteCollectionId,
+                queries,
+                 
+
+            )
+        } catch (error) {
+            console.log('Appwrite Error ::getPosts :: error',error );
+            return false
+        }
+    }
+    // file upload 
+    async uploadFile(file){
+        try {
+            return await this.buket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+         } catch (error) {
+            console.log('Appwrite Error ::uploadFile :: error',error );
+            return false
+        }
+    }
+    // file delete
+
+     async deleteFile(fileId){
+        try {
+            return await this.buket.deleteFile(
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log('Appwrite Error ::deleteFile :: error',error );
+            return false
+        }
+     }
+     // file preview
+        getFilePreview(fileId){
+            return  this.buket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+        }
+        
 }
 
 
