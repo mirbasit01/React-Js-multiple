a76d170555bd37d587f5bbcea716fdb89801a65d
Abdul Basit <121716860+mirbasit01@users.noreply.github.com>
2025-02-11 00:58:27 +0500

    theme

---
+++ themeTogle/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="p-4 bg-pink-600">Abdul Basit</h1>
+    </>
+  );
+}
+
+export default App;
